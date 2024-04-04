ebfb70762055179b466cdefbe5d434fb0883d0e1
Bri Halterman <halterma@gmail.com>
2024-04-04 12:13:23 -0400

    .concat()

---
+++ concat.js
@@ -0,0 +1,6 @@
+const users = ['Brandon', 'Keeli', 'Tiffany', 'Steve'];
+const newUsers = ['Yamil', 'Beth', 'Caleb'];
+
+let userList = users.concat(newUsers);
+console.log(userList);
+// returns ['Brandon', 'Keeli', 'Tiffany', 'Steve', 'Yamil', 'Beth', 'Caleb']
