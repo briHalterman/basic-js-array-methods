b9819af36312f3ec4081419f5242533c4fb76d9f
Bri Halterman <halterma@gmail.com>
2024-04-04 13:33:42 -0400

    .forEach()

---
+++ forEach.js
@@ -0,0 +1,18 @@
+const users = ['Brandon', 'Keeli', 'Tiffany', 'Steve'];
+
+users.forEach( (user, index) => {
+  console.log(user);
+  console.log(index);
+})
+
+// returns 'Brandon'
+// returns 0
+
+// returns 'Keeli'
+// returns 1
+
+// returns 'Tiffany'
+// returns 2
+
+// returns 'Steve'
+// returns 3
